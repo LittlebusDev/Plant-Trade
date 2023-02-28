727545a9d955a75536b954ef887c40b12bbc7f49
Littlebus <tori.stevens13@gmail.com>
2023-02-28 14:51:03 +1300

    Navbar test

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+     <nav className="nav">
+        <a href="/Homepage" img src="./src/Components/Images/wordMark.png">
+          Plantrade</a>
+      <ul>
+          <li> <a href="/Notifications">Notifications</a></li>
+          <li> <a href="/Profile">Profile</a></li>
+      </ul>
+     </nav> 
+    )
+  }
+  
+  export default Navbar
+  
