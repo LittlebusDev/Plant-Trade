0630e83c2b4e08d6802c26be6162f9bcd9e95af3
Littlebus <tori.stevens13@gmail.com>
2023-02-28 15:34:13 +1300

    Adjustments to navbar
    Display navbar (app.js now returns navbar in previewer, when it didn't before)

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,8 @@
 const Navbar = () => {
     return (
      <nav className="nav">
-        <a href="/Homepage" img src="./src/Components/Images/wordMark.png">
-          Plantrade</a>
+        <a href="/Homepage"> <img src=""></img>
+        Plantrade</a>
       <ul>
           <li> <a href="/Notifications">Notifications</a></li>
           <li> <a href="/Profile">Profile</a></li>
