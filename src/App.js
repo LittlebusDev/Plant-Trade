eae81d95f31ba1c551695b7052e1c1c18a8f7aca
KeumjuOk <bijouok7@gmail.com>
2023-03-07 16:10:36 +1300

    signin/up form validation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import { useState } from "react";
-import './Styles/App.scss';
-import Navbar from './Components/Navbar';
-import Hero from './Components/Hero';
-import SearchBar from './Components/SearchBar';
-import MessageBubble from './Components/MessageBubble';
-import Discussion from './Components/Discussion';
-import ListingDetails from './Components/ListingDetails';
-import Upload from './Components/Upload';
-import Header from './Components/Header';
+import { useState, useEffect } from "react";
+import "./Styles/App.scss";
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import SearchBar from "./Components/SearchBar";
+import MessageBubble from "./Components/MessageBubble";
+import Discussion from "./Components/Discussion";
+import ListingDetails from "./Components/ListingDetails";
+import Upload from "./Components/Upload";
+import Header from "./Components/Header";
 import Categories from "./Components/Categories";
 import Divider from "./Components/Divider";
 import Card from "./Components/Card";
 import ViewAllListings from "./Components/ViewAllListings";
+import Signin from "../src/Components/Signin";
 
 function App() {
-  
-  return ( 
-  <>
-  <Navbar />
-
-
-  </>
-  )
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
 }
 
 export default App;
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,32 +1,105 @@
-import React, { useState } from "react";
-import './styles.css';
+import React, { useState, useEffect} from "react";
+import "./Styles/App.scss";
 
+function Register(props) {
+ 
+        const [formErrors, setFormErrors] = useState({});
+        const [isSubmit, setIsSubmit] = useState(false);
+        const [name, setName] = useState("");
+        const [email, setEmail] = useState("");
+        const [password, setPassword] = useState("");
+      
+        const handleSubmit = (e) => {
+          e.preventDefault();
+          setFormErrors(validate(email));
+          setIsSubmit(true);
+        };
+      
+        useEffect(() => {
+          console.log(formErrors);
+          if (Object.keys(formErrors).length === 0 && isSubmit) {
+            console.log(email);
+          }
+        }, [formErrors]);
+      
+        const validate = (values) => {
+          const errors = {};
+          const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+      
+          if (!values.name) {
+            errors.name = "Full name is required!";
+          } if (!values.email) {
+            errors.email = "Email is required!";
+          } else if (!regex.test(values.email)) {
+            errors.email = "This is not a valid email format!";
+          }
+          if (!values.password) {
+            errors.password = "Password is required!";
+          } else if (values.password.length < 4) {
+            errors.password = "Password must be more than 4 characters! ";
+          } else if (values.password.length > 10) {
+            errors.password = "Password cannot exceed more than 10 characters! ";
+          }
+          return errors;
+        };
+      
+        return (
+          <div className="auth-form-container">
+            <h2>Register</h2>
+      
+            <form className="register-form" onSubmit={handleSubmit}>
+              <label className="auth-label" htmlFor="name">
+                Full name
+              </label>
+              <input 
+              type = "text"
+              value={name} 
+              onChange={(e) => setName(e.target.value)} 
+              name="name" 
+              id="name" 
+              placeholder="Full name" />
+              <p>{formErrors.name}</p>
+      
+              <label className="auth-label" htmlFor="email">
+                Email
+              </label>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                // onChange={handleChange}
+                type="text"
+                placeholder="youremail@gmail.com"
+                id="email"
+                name="email"
+              />
+              <p>{formErrors.email}</p>
+      
+              <label className="auth-label" htmlFor="password">
+                Password
+              </label>
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                // onChange={handleChange}
+                type="password"
+                placeholder="**********"
+                id="password"
+                name="password"
+              />
+              <p>{formErrors.password}</p>
+      
+              <button type="submit">Sign Up</button>
+      
+              <button
+                className="link-btn"
+                onClick={() => props.onFormSwitch("signin")}
+              >
+                Aleady have an accout? Login here.
+              </button>
+            </form>
+          </div>
+        );
+      };
+      
 
-export const Register = (props) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [name, setName] = useState('');
-
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        console.log(email);
-    }
-
-    return (
-        <div className="auth-form-container">
-             <h2>Register</h2>
-          <form className="register-form" onSubmit={handleSubmit}>
-          <label className = "auth-label" htmlFor="name">Full name</label>
-          <input value={name} name="name" id="name" placeholder="Full name" />
-          <label className = "auth-label" htmlFor="email">Email</label>
-          <input value={email} onChange = {(e) => setEmail(e.target.value)} type="email" placeholder = 'youremail.gmail.com' id="email" name="email"/>
-          <label className = "auth-label" htmlFor="password">Password</label>
-          <input value={password} onChange = {(e) => setPassword(e.target.value)}type="password" placeholder = "**********" id="password" name="password"/>
-          <button type="submit">Log In</button>
-     
-          </form>
-          <button className="link-btn" onClick = {()=> props.onFormSwitch('signin')}>Aleady have an accout? Login here.</button>
-      </div>
-    
-    )
-}
+export default Register;
